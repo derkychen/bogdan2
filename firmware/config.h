ad4305a37a33a13e1c1201ea981e15a9b0dbf092
Derek Chen <derekjchen@proton.me>
2026-06-08 14:51:00 +0200

    feat(firmware): hardware config interfacing code

---
+++ firmware/config.h
@@ -0,0 +1,28 @@
+/**
+ * @brief Interface between physical configuration of circuitry and code.
+ */
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define NUM_GPIO 30
+
+#define X_TRIGGER_IN_GPIO 18
+#define X_ANALOG_IN_GPIO 16
+#define X_TRIGGER_OUT_GPIO 20
+
+#define Y_TRIGGER_IN_GPIO 19
+#define Y_ANALOG_IN_GPIO 17
+#define Y_TRIGGER_OUT_GPIO 21
+
+#define PULSE_TRIGGER_GPIO 22
+
+/**
+ * @brief Initialize hardware configurations (i.e. pins, pull up/down, etc.).
+ *
+ * This function will only work as expected if the pins as defined above support
+ * the functionality they are supposed to provide.
+ */
+void config_init();
+
+#endif
