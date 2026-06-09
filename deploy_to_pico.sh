8988d9ec534396a2b193c7667e8e3a6ea4542a00
Derek Chen <derekjchen@proton.me>
2026-06-09 10:39:33 +0200

    chore: figured out location of pico on macos and windows

--- deploy_to_pico.sh
+++ deploy_to_pico.sh
@@ -1,20 +1,22 @@
-#!/bin/bash
+#!/usr/bin/bash
 #
 # Build and copy the UF2 to the Pico.
 
 set -euo pipefail
 
-FLAG="${1:-}"
+OPTION="${1:-}"
 
 export PICO_SDK_PATH="${PICO_SDK_PATH:-$HOME/.pico/pico-sdk}"
 
 cd firmware
 
-if [[ "$FLAG" == "--clear-build" ]]; then
+# Optionally clear the `build/` directory
+if [[ "$OPTION" == "--clear-build" ]]; then
   rm -rf build
 fi
 
 cmake -S . -B build -DCMAKE_BUILD_TYPE=Release
 cmake --build build --target bogdan2
 
-# cp build/bogdan2.uf2 /RPI-RP2/bogdan2.uf2 # TODO: Verify location of Pico
+cp build/bogdan2.uf2 /e/ # On Windows
+# cp build/bogdan2.uf2 /Volumes/RP2350 # On macOS/Linux
