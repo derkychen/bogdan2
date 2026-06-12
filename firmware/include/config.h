0d35492e3d261d39d4831501dd4c175191b58883
Derek Chen <derekjchen@proton.me>
2026-06-12 12:47:53 +0200

    feat(firmware): add adc configs

--- firmware/include/config.h
+++ firmware/include/config.h
@@ -1,16 +1,26 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+// WARNING: Do not change these values.
+#define ADC_MIN_VAL 0u
+#define ADC_MAX_VAL 4095u
+#define PWM_MIN_VAL 0u
+#define PWM_MAX_VAL 65535u
+
 // WARNING: Changing any of these values requires changing of wiring as well.
-#define X_TRIGGER_IN_GPIO  18
-#define X_ANALOG_IN_GPIO   16
-#define X_TRIGGER_OUT_GPIO 20
+#define X_TRIGGER_IN_GPIO    18u
+#define X_ANALOG_IN_GPIO     16u
+#define X_TRIGGER_OUT_GPIO   20u
+#define X_ANALOG_OUT_GPIO    26u
+#define X_ANALOG_OUT_CHANNEL 0u
 
-#define Y_TRIGGER_IN_GPIO  19
-#define Y_ANALOG_IN_GPIO   17
-#define Y_TRIGGER_OUT_GPIO 21
+#define Y_TRIGGER_IN_GPIO    19u
+#define Y_ANALOG_IN_GPIO     17u
+#define Y_TRIGGER_OUT_GPIO   21u
+#define Y_ANALOG_OUT_GPIO    27u
+#define Y_ANALOG_OUT_CHANNEL 1u
 
-#define PULSE_TRIGGER_GPIO 22
+#define PULSE_TRIGGER_GPIO 22u
 
 /** @brief Initialize hardware configuration.
  *
--- firmware/src/config.c
+++ firmware/src/config.c
@@ -1,43 +1,52 @@
 #include "config.h"
+#include "hardware/adc.h"
 #include "hardware/gpio.h"
 #include "hardware/pwm.h"
 
 void
 config_init (void)
 {
-    // Trigger IN for x-axis controller
+    adc_init();
+
+    // Configure Trigger IN for x-axis controller.
     gpio_init(X_TRIGGER_IN_GPIO);
     gpio_set_dir(X_TRIGGER_IN_GPIO, GPIO_OUT);
     gpio_put(X_TRIGGER_IN_GPIO, 0);
 
-    // Analog IN for x-axis controller
+    // Configure Analog IN for x-axis controller.
     gpio_set_function(X_ANALOG_IN_GPIO, GPIO_FUNC_PWM);
     uint x_analog_in_slice = pwm_gpio_to_slice_num(X_ANALOG_IN_GPIO);
     pwm_set_wrap(x_analog_in_slice, 65535);
     pwm_set_enabled(x_analog_in_slice, true);
 
-    // Trigger OUT for x-axis controller
+    // Configure Trigger OUT for x-axis controller.
     gpio_init(X_TRIGGER_OUT_GPIO);
     gpio_set_dir(X_TRIGGER_OUT_GPIO, GPIO_IN);
     gpio_pull_down(X_TRIGGER_OUT_GPIO);
 
-    // Trigger IN for y-axis controller
+    // Configure Analog OUT for x-axis controller.
+    adc_gpio_init(X_ANALOG_OUT_GPIO);
+
+    // Configure Trigger IN for y-axis controller.
     gpio_init(Y_TRIGGER_IN_GPIO);
     gpio_set_dir(Y_TRIGGER_IN_GPIO, GPIO_OUT);
     gpio_put(Y_TRIGGER_IN_GPIO, 0);
 
-    // Analog IN for x-axis controller
+    // Configure Analog IN for x-axis controller.
     gpio_set_function(Y_ANALOG_IN_GPIO, GPIO_FUNC_PWM);
     uint y_analog_in_slice = pwm_gpio_to_slice_num(Y_ANALOG_IN_GPIO);
     pwm_set_wrap(x_analog_in_slice, 65535);
     pwm_set_enabled(x_analog_in_slice, true);
 
-    // Trigger OUT for x-axis controller
+    // Configure Trigger OUT for x-axis controller.
     gpio_init(Y_TRIGGER_OUT_GPIO);
     gpio_set_dir(Y_TRIGGER_OUT_GPIO, GPIO_IN);
     gpio_pull_down(Y_TRIGGER_OUT_GPIO);
+    
+    // Configure Analog OUT for y-axis controller.
+    adc_gpio_init(Y_ANALOG_OUT_GPIO);
 
-    // Pulse counting input
+    // Configure pulse counting input.
     gpio_init(PULSE_TRIGGER_GPIO);
     gpio_set_dir(PULSE_TRIGGER_GPIO, GPIO_IN);
     gpio_pull_down(PULSE_TRIGGER_GPIO);
