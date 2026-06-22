f52959f6e8d947e890399145d3fbd94910009523
jadrouu <rou3099@gmail.com>
2026-06-22 15:37:08 +0200

    feat(firmware): Add controller digital I/O support (Trigger IN/OUT)

---
+++ firmware/platform/samd21g18a/controller_digital.h
@@ -0,0 +1,16 @@
+#ifndef CONTROLLER_DIGITAL_H
+#define CONTROLLER_DIGITAL_H
+
+#include "controller.h"
+#include <stdbool.h>
+
+void controller_configure_trigger_in(const Controller *controller)
+void controller_configure_capture_trigger(const Controller *controller)
+
+void controller_pulse_trigger_in(const Controller *controller);
+bool controller_read_capture_trigger(const Controller *controller);
+
+void and_gate_configure_output(void);
+void and_gate_write_signal(bool high)
+
+#endif
