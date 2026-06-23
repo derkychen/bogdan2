7eca3cd3692f759fdce37a865ecc17caec909f26
Derek Chen <derekjchen@proton.me>
2026-06-23 10:47:12 +0200

    feat(firmware): cmake utils and wrappers for external stuff

---
+++ firmware/cmake/external/tinyusb_device_samd.cmake
@@ -0,0 +1,24 @@
+add_library(tinyusb_device_samd STATIC)
+
+target_sources(tinyusb_device_samd PRIVATE
+  "${EXTERNAL_DIR}/tinyusb/src/tusb.c"
+  "${EXTERNAL_DIR}/tinyusb/src/common/tusb_fifo.c"
+  "${EXTERNAL_DIR}/tinyusb/src/device/usbd.c"
+  #"${TINYUSB_DIR}/src/device/usbd_control.c"
+  "${EXTERNAL_DIR}/tinyusb/src/class/cdc/cdc_device.c"
+  "${EXTERNAL_DIR}/tinyusb/src/portable/microchip/samd/dcd_samd.c"
+)
+
+target_include_directories(tinyusb_device_samd SYSTEM PUBLIC
+  "${EXTERNAL_DIR}/tinyusb/src"
+)
+
+target_link_libraries(tinyusb_device_samd
+  PRIVATE
+    mcu::samd21g18a
+  PUBLIC
+    config
+    external::cmsis_atmel_samd21g18a
+)
+
+add_library(external::tinyusb_device_samd ALIAS tinyusb_device_samd)
