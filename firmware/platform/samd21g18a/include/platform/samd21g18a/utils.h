9828ab68587fd4662975b3da9e6847edd775bd0f
Derek Chen <derekjchen@proton.me>
2026-06-23 11:11:14 +0200

    fix(firmware): inlining gclk polling

--- firmware/platform/samd21g18a/include/platform/samd21g18a/utils.h
+++ firmware/platform/samd21g18a/include/platform/samd21g18a/utils.h
@@ -1,7 +1,18 @@
 #ifndef PLATFORM_SAMD21G18A_UTILS_H
 #define PLATFORM_SAMD21G18A_UTILS_H
 
+#include "samd21g18a.h"
+
 /** @brief Poll the GCLK register until it is synchronized. */
-inline void platform_samd21g18a_poll_gclk_until_synchronized(void);
+static inline void
+platform_samd21g18a_poll_gclk_until_synchronized (void)
+{
+
+    while (GCLK->STATUS.bit.SYNCBUSY)
+    {
+    }
+
+    return;
+}
 
 #endif
