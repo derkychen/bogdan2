2473edd90b8e82f10aa11b3a5268f3ad97c9a6b4
Derek Chen <derekjchen@proton.me>
2026-07-03 16:04:38 +0200

    fix(firmware): revamp platform digital code because industruino digital expander cannot do 5V logic

---
+++ firmware/platform/samd21g18a/src/digital.c
@@ -0,0 +1,45 @@
+#include "platform/samd21g18a/digital.h"
+#include "samd21g18a.h"
+
+void
+platform_samd21g18a_digital_pin_direction_set_output (
+    platform_samd21g18a_pin_t const *pin)
+{
+    PORT->Group[pin->port_group].DIRSET.reg = (1ul << pin->number);
+}
+void
+platform_samd21g18a_digital_pin_direction_set_input (
+    platform_samd21g18a_pin_t const *pin)
+{
+    PORT->Group[pin->port_group].DIRCLR.reg = (1ul << pin->number);
+}
+
+void
+platform_samd21g18a_digital_pin_level_set_low (
+    platform_samd21g18a_pin_t const *pin)
+{
+    PORT->Group[pin->port_group].OUTCLR.reg = (1ul << pin->number);
+
+    return;
+}
+
+void
+platform_samd21g18a_digital_pin_level_set_high (
+    platform_samd21g18a_pin_t const *pin)
+{
+    PORT->Group[pin->port_group].OUTSET.reg = (1ul << pin->number);
+
+    return;
+}
+
+platform_samd21g18a_digital_level_t
+platform_samd21g18a_digital_pin_read (platform_samd21g18a_pin_t const *pin)
+{
+    // Enable input buffer.
+    PORT->Group[pin->port_group].PINCFG[pin->number].bit.INEN = 1;
+
+    // Read pin state.
+    return ((PORT->Group[pin->port_group].IN.reg & (1ul << pin->number)) != 0)
+               ? PLATFORM_SAMD21G18A_DIGITAL_LEVEL_HIGH
+               : PLATFORM_SAMD21G18A_DIGITAL_LEVEL_LOW;
+}
