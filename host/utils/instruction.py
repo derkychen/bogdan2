6591566681586444c3e815fcea5e5577d677806a
Derek Chen <derekjchen@proton.me>
2026-07-20 18:20:50 +0200

    feat(host): utils, mainly instruction parsing for now

--- host/utils/instruction.py
+++ host/utils/instruction.py
@@ -2,9 +2,15 @@
 
 from typing import Any, Final
 
-MOVEMENT_MODE_POINT: Final[int] = 0
-MOVEMENT_MODE_TIME: Final[int] = 0
-MOVEMENT_MODE_CONTINUOUS: Final[int] = 0
+MODE_POINT: Final[int] = 0
+MODE_TIME: Final[int] = 0
+MODE_CONTINUOUS: Final[int] = 0
+
+MODE_MAP = {
+    "point": MODE_POINT,
+    "time": MODE_TIME,
+    "continuous": MODE_CONTINUOUS,
+}
 
 
 class InvalidModeException(Exception):
@@ -32,32 +38,14 @@ def check_no_none(instruction: dict[str, Any]) -> None:
             check_no_none(val)
 
 
-def parse_indio_instruction(instruction: dict[str, Any]) -> dict[str, int]:
+def mcu_instruction(instruction: dict[str, Any]) -> dict[str, int]:
     """Parse instructions into a flat JSON for the Industruino IND.I/O."""
     indio_instruction = {}
 
-    mode = instruction["mode"]
-
-    if type(mode is str):
-        match mode.lower():
-            case "point":
-                indio_instruction["mode"] = MOVEMENT_MODE_POINT
-            case "time":
-                indio_instruction["mode"] = MOVEMENT_MODE_TIME
-            case "continuous":
-                indio_instruction["mode"] = MOVEMENT_MODE_CONTINUOUS
-            case _:
-                raise InvalidModeException("Mode provided is not valid.")
-
-    elif type(mode is int) and mode in (
-        MOVEMENT_MODE_POINT,
-        MOVEMENT_MODE_TIME,
-        MOVEMENT_MODE_CONTINUOUS,
-    ):
-        indio_instruction["mode"] = mode
-
-    else:
-        raise Exception("Mode provided is not valid.")
+    try:
+        indio_instruction["mode"] = MODE_MAP[instruction["mode"].lower()]
+    except KeyError as err:
+        raise InvalidModeException("Mode provided is not valid.") from err
 
     indio_instruction["x_min"] = instruction["grid"]["x"]["min"]
     indio_instruction["x_max"] = instruction["grid"]["x"]["max"]
