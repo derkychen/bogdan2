ad967247a7a2c424b78cac0e7658ac3e1cf32379
Derek Chen <derekjchen@proton.me>
2026-07-21 17:15:55 +0200

    feat(host): updated instruction parsing

--- host/utils/instruction.py
+++ host/utils/instruction.py
@@ -12,12 +12,12 @@ def _ceil_div(num: int, denom: int) -> int:
     return -(num // -denom)
 
 
-def check_no_none(obj: dict[str, Any]) -> None:
+def check_no_none(obj: Any) -> None:
     """Traverse all instruction fields and check that they are not `None`."""
     if isinstance(obj, dict):
         for key, val in obj.items():
             if val is None:
-                raise ValueError(f"{key} is None")
+                raise InstructionFieldNone(f"'{key}' is None")
             check_no_none(val)
 
     elif isinstance(obj, list):
@@ -29,7 +29,9 @@ def mcu_instruction(instruction: dict[str, Any]) -> dict[str, int]:
     """Parse instructions into a flat JSON for the Industruino IND.I/O."""
     indio_instruction = {}
 
-    indio_instruction["mode"] = instruction["mode"]
+    mode = instruction["mode"]
+    indio_instruction["mode"] = mode
+
     indio_instruction["x_min"] = instruction["grid"]["x"]["min"]
     indio_instruction["x_max"] = instruction["grid"]["x"]["max"]
     indio_instruction["x_unit_nm"] = instruction["grid"]["x"]["unit_nm"]
@@ -38,10 +40,19 @@ def mcu_instruction(instruction: dict[str, Any]) -> dict[str, int]:
     indio_instruction["y_max"] = instruction["grid"]["y"]["max"]
     indio_instruction["y_unit_nm"] = instruction["grid"]["y"]["unit_nm"]
     indio_instruction["y_origin_nm"] = instruction["grid"]["y"]["origin_nm"]
-    indio_instruction["num_pulses"] = instruction["capture"]["num_pulses"]
-    indio_instruction["posttrigger_time_us"] = _ceil_div(
-        instruction["capture"]["posttrigger_time_ns"], 1000
-    )
+
+    match mode:
+        case "point_count":
+            indio_instruction["num_pulses"] = instruction["capture"][
+                "num_pulses"
+            ]
+            indio_instruction["posttrigger_time_us"] = _ceil_div(
+                instruction["capture"]["posttrigger_time_ns"], 1000
+            )
+        case "point_time":
+            indio_instruction["wait_time_ms"] = instruction["capture"][
+                "wait_time_ms"
+            ]
 
     check_no_none(indio_instruction)
 
