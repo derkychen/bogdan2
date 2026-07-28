583ba1b241b2995103e057fe797ed0a204e32858
Derek Chen <derekjchen@proton.me>
2026-07-28 17:35:13 +0200

    feat(firmware): utils

--- firmware/platform/samd21g18a/include/platform/samd21g18a/utils.h
+++ firmware/platform/samd21g18a/include/platform/samd21g18a/utils.h
@@ -1,17 +1,16 @@
 #ifndef PLATFORM_SAMD21G18A_UTILS_H
 #define PLATFORM_SAMD21G18A_UTILS_H
 
-#include "samd21g18a.h"
+#include <stdint.h>
 
 /** @brief Poll the GCLK until it is synchronized. */
-static inline void
-platform_samd21g18a_utils_gclk_poll_sync (void)
-{
-    while (GCLK->STATUS.bit.SYNCBUSY)
-    {
-    }
+void platform_samd21g18a_utils_gclk_poll_sync(void);
 
-    return;
-}
+/**
+ * @brief Enable a peripheral with GCLK0.
+ *
+ * NOTE: This function assumes that GCLK0 has already been configured.
+ */
+void platform_samd21g18a_utils_gclk0_enable(uint16_t id);
 
 #endif
---
+++ firmware/platform/samd21g18a/src/utils.c
@@ -0,0 +1,25 @@
+#include "platform/samd21g18a/utils.h"
+#include "sam.h" // IWYU pragma: keep
+
+void
+platform_samd21g18a_utils_gclk_poll_sync (void)
+{
+    while (GCLK->STATUS.bit.SYNCBUSY)
+    {
+    }
+
+    return;
+}
+
+void
+platform_samd21g18a_utils_gclk0_enable (uint16_t id)
+{
+    GCLK->CLKCTRL.reg
+        = (uint16_t)(id | GCLK_CLKCTRL_GEN_GCLK0 | GCLK_CLKCTRL_CLKEN);
+
+    while (GCLK->STATUS.bit.SYNCBUSY != 0U)
+    {
+    }
+
+    return;
+}
